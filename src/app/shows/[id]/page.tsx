e5120cfb12eba9a7cfcf602f1e6d6bceb98c4918
Newton-Nganga <nganga7newt@gmail.com>
2023-07-24 22:54:57 +0300

    passes shows data to the carousel item

--- src/app/shows/[id]/page.tsx
+++ src/app/shows/[id]/page.tsx
@@ -1,36 +1,199 @@
-"use client"
+"use client";
+import React, { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
-
 import InnerPage from "@/components/Pages/InnerPages";
 import { BsStarFill, BsStar } from "react-icons/bs";
 import { RiHeartFill, RiShareLine, RiVolumeMuteFill } from "react-icons/ri";
 import MoviesCarousel from "@/components/MoviesCarousel";
-
+import axios, { AxiosRequestConfig } from "axios";
 type Props = {};
 
-export default function Page({}: Props) {
+//movie details
+interface MovieData {
+  genres: { id: number; name: string }[];
+  id: number;
+  imdb_id: string;
+  overview: string;
+  popularity: number;
+  release_date: string;
+  status: string;
+  tagline: string;
+  title: string;
+  vote_average: number;
+  runtime: any;
+  poster: string;
+  backdrop_path: string;
+}
+interface TrailerData {
+  id: string;
+  site: string;
+  key: string;
+  official: boolean;
+  size: number;
+  name: string;
+  type: string;
+  iso_3166_1: string;
+  iso_639_1: string;
+  published_at: string;
+}
+interface Movies {
+  title: string;
+  id: number;
+  backdrop_path: string;
+  release_date: any;
+  poster_path: string;
+}
+
+export default function Page({ params }: { params: { id: number } }) {
   //const breadcrumb = useBreadcrumb();
   const pathname = usePathname();
-  console.log(pathname);
+  //console.log(params.id);
+
+  const [movieData, setMovieData] = useState<MovieData | null>(null);
+  const [trailerData, setTrailerData] = useState<TrailerData[] | null>(null);
+  const [suggested, setSuggested] = useState<Movies[] | null>(null);
+  const [upcomingMovies, setUpcomingMovies] = useState<Movies[] | null>(null);
+  const suggestedShowsOptions: AxiosRequestConfig = {
+    method: "GET",
+    url: `${process.env.BASE_ENDPOINT}discover/tv`,
+    headers: {
+      accept: "application/json",
+      Authorization: process.env.API_AUTHORIZATION_TOKEN,
+    },
+  };
+  const options: AxiosRequestConfig = {
+    method: "GET",
+    url: `${process.env.BASE_ENDPOINT}tv/${params.id}`,
+    headers: {
+      accept: "application/json",
+      Authorization: process.env.API_AUTHORIZATION_TOKEN,
+    },
+  };
+  const trailerOptions: AxiosRequestConfig = {
+    method: "GET",
+    url: `${process.env.BASE_ENDPOINT}tv/${params.id}/videos`,
+    headers: {
+      accept: "application/json",
+      Authorization: process.env.API_AUTHORIZATION_TOKEN,
+    },
+  };
+
+  const requestOptions: AxiosRequestConfig = {
+    method: "GET",
+    url: `${process.env.BASE_ENDPOINT}movie/upcoming`,
+    headers: {
+      accept: "application/json",
+      Authorization: process.env.API_AUTHORIZATION_TOKEN,
+    },
+  };
+  useEffect(() => {
+    async function getTrending() {
+      try {
+        const response = await axios.request(options);
+        const suggestedResponse = await axios.request(suggestedShowsOptions);
+        const trailerResponse = await axios.request(trailerOptions);
+        // console.log(trailerResponse)
+
+        const {
+          adult,
+          spoken_languages,
+          video,
+          poster_path,
+          budget,
+          belongs_to_collection,
+          homepage,
+          original_title,
+          original_language,
+          production_countries,
+          production_languages,
+          vote_count,
+          revenue,
+          ...rest
+        }: any = response.data;
+
+        const movieData: MovieData = rest;
+
+        const trailer_data: TrailerData[] = trailerResponse.data.results.filter(
+          (data: any) =>
+            // data.name === "Official Trailer" &&
+            data.official && data.site === "YouTube"
+        );
+        const upcomingResponse = await axios.request(requestOptions);
+
+        const upcoming_data: Movies[] = upcomingResponse.data.results.map(
+          ({
+            title,
+            id,
+            backdrop_path,
+            poster_path,
+            release_date,
+            ...others
+          }: any) => {
+            return {
+              title,
+              id,
+              backdrop_path,
+              poster_path,
+              release_date,
+            } as Movies;
+          }
+        );
+        setUpcomingMovies(upcoming_data);
+        //   console.log("popular response", popularResponse);
+        const suggested_data: Movies[] = suggestedResponse.data.results.map(
+          ({
+            name: title,
+            id,
+            backdrop_path,
+            poster_path,
+            release_date,
+            ...others
+          }: any) => {
+            return {
+              title,
+              id,
+              backdrop_path,
+              poster_path,
+              release_date,
+            } as Movies;
+          }
+        );
+        //c
+        setMovieData(movieData);
+        setTrailerData(trailer_data);
+        setSuggested(suggested_data);
+        setUpcomingMovies(upcomingMovies)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getTrending();
+    //eslint-disable-next-line
+  }, []);
+  if (trailerData && trailerData.length > 0) {
+    console.log("data", trailerData[trailerData.length - 1]);
+  }
+
   return (
     <InnerPage>
       <section>
-        <video width="100%" height="400" controls>
-          <source src="https://youtu.be/GJELT-Ng8rw" type="video/mp4" />
-        </video>
+      <iframe
+        width="100%"
+        height="500"
+        src={`https://www.youtube.com/embed${
+          (trailerData && trailerData.length > 0 )&& trailerData[trailerData.length -1]
+        }`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture,full-screen"
+      ></iframe>
         <section className="section">
           <div className="inner-section flex flex-col border-slice py-4 gap-4">
             <h4 className="texture my-4 w-fit mr-auto text-2xl font-extrabold">
-              The black panther
+              {movieData?.title}
             </h4>
             <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using Content here, content
-              here, making it look like readable English. Many desktop
-              publishing packages and web page editors now use Lorem Ipsum as
-              their default model text.
+              {movieData?.overview}
             </p>
             <div className="flex items-center gap-4 my-3">
               <p className="flex gap-1 text-red-600">
@@ -40,29 +203,22 @@ export default function Page({}: Props) {
                 <BsStarFill />
                 <BsStar />
               </p>
-              <span>8.0(imdb)</span>
+              <span>{movieData?.vote_average}(imdb)</span>
             </div>
             <div>
               <div className="text-sm md:text-lg">
                 <p>
-                  <span className="text-red-600 font-semibold">Actor:</span>{" "}
-                  Josh Duhamel,Bruce Willis,Rosario Dawson
-                </p>
-                <p>
-                  <span className="text-red-600 font-semibold">Genres:</span>{" "}
-                  Josh Action,Drama,Thriller,Crime
-                </p>
-                <p>
-                  <span className="text-red-600 font-semibold">Director:</span>{" "}
-                  Josh David Barrett
+                  <span className="text-red-600 font-semibold">Tagline:</span>{" "}
+                 {movieData?.tagline}
                 </p>
                 <p>
                   <span className="text-red-600 font-semibold">Duration:</span>{" "}
-                  Josh 2h 10m
+                  {new Date(movieData?.runtime).getHours()}Hrs
+                  {new Date(movieData?.runtime).getMinutes()}Min
                 </p>
                 <p>
-                  <span className="text-red-600 font-semibold">Quality:</span>
-                  <span className="px-2 bg-red-500 rounded-md">HD</span>
+                  <span className="text-red-600 font-semibold">Popularity:</span>
+                  <span className="px-2 bg-red-500 rounded-md">{movieData?.popularity}</span>
                 </p>
               </div>
             </div>
@@ -85,8 +241,8 @@ export default function Page({}: Props) {
             </div>
           </div>
         </section>
-        <MoviesCarousel title={'You May Also Like'}/>
-        <MoviesCarousel title={'Upcoming Movies'}/>
+        <MoviesCarousel movies={suggested} title={"You May Also Like"} />
+        <MoviesCarousel movies={upcomingMovies} title={"Upcoming Movies"} />
       </section>
     </InnerPage>
   );
--- src/components/movieCarouselItems/SuggestedShows.tsx
+++ src/components/movieCarouselItems/SuggestedShows.tsx
@@ -3,19 +3,17 @@ import React, { useState, useEffect } from "react";
 import MoviesCarousel from "../MoviesCarousel";
 import axios, { AxiosRequestConfig } from "axios";
 
-type Props = {
-
-};
+type Props = {};
 
 interface Movies {
   title: string;
   id: number;
   backdrop_path: string;
-  poster_path:string
+  poster_path: string;
   release_date: any;
 }
 
-export default function SuggestedShows({  }: Props) {
+export default function SuggestedShows({}: Props) {
   const [suggested, setSuggested] = useState<Movies[] | null>(null);
   const suggestedShowsOptions: AxiosRequestConfig = {
     method: "GET",
@@ -25,36 +23,49 @@ export default function SuggestedShows({  }: Props) {
       Authorization: process.env.API_AUTHORIZATION_TOKEN,
     },
   };
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     async function getSimilar() {
-    // console.log('popular function started')
-    try {
-    //   console.log("trying ...")
-      const suggestedResponse = await axios.request(suggestedShowsOptions);
-      
-    //   console.log("popular response", popularResponse);
-      const suggested_data: Movies[] = suggestedResponse.data.results.map(
-        ({ name:title, id, backdrop_path,poster_path, release_date, ...others }: any) => {
-          return { title, id,backdrop_path , poster_path, release_date } as Movies;
-        }
-      );
-      //console.log("popular shows",popular_data);
-      
+      // console.log('popular function started')
+      try {
+        //   console.log("trying ...")
+        const suggestedResponse = await axios.request(suggestedShowsOptions);
 
-      setSuggested(suggested_data);
-    } catch (error) {
-      console.log(error);
+        //   console.log("popular response", popularResponse);
+        const suggested_data: Movies[] = suggestedResponse.data.results.map(
+          ({
+            name: title,
+            id,
+            backdrop_path,
+            poster_path,
+            release_date,
+            ...others
+          }: any) => {
+            return {
+              title,
+              id,
+              backdrop_path,
+              poster_path,
+              release_date,
+            } as Movies;
+          }
+        );
+        //console.log("popular shows",popular_data);
+
+        setSuggested(suggested_data);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
-  getSimilar();
-  //eslint-disable-next-line
-  },[])
+    getSimilar();
+    //eslint-disable-next-line
+  }, []);
 
+  //   if (popular && popular.length > 0) {
+  //     console.log("popular ->", popular);
+  //   }
 
-//   if (popular && popular.length > 0) {
-//     console.log("popular ->", popular);
-//   }
-
-  return <MoviesCarousel title={"Suggested Shows For You"} movies={suggested} />;
+  return (
+    <MoviesCarousel title={"Suggested Shows For You"} movies={suggested} />
+  );
 }
