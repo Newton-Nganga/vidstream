f2f5db83d60de124682da7b2cfd5a68f8d21f5e8
Newton-Nganga <nganga7newt@gmail.com>
2023-08-04 16:15:56 +0300

    added nonce to header

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const { randomBytes } = require('crypto')
 /** @type {import('next').NextConfig} */
 const nounce = randomBytes(128).toString('base64')
 const nextConfig = {
-  async headers(){
-     return [
-      { source: '/movie/:id*', headers: [{ 
-        key: "Content-Security-Policy",
-        value:`default-src 'self'; script-src 'self' 'unsafe-inline' 'nonce-${nounce}' https://www.youtube.com/ https://www.multiembed.mov/ https://www.2embed.cc; frame-src https://www.youtube.com/ https://www.multiembed.mov/ https://www.2embed.cc; style-src 'self' 'unsafe-inline';`
-      }]}
-     ]
-  },
+  // async headers(){
+  //    return [
+  //     { source: '/movie/:id*', headers: [{ 
+  //       key: "Content-Security-Policy",
+  //       value:`default-src 'self'; script-src 'self' 'unsafe-inline' 'nonce-${nounce}' https://www.youtube.com/ https://www.multiembed.mov/ https://www.2embed.cc; frame-src https://www.youtube.com/ https://www.multiembed.mov/ https://www.2embed.cc; style-src 'self' 'unsafe-inline';`
+  //     }]}
+  //    ]
+  // },
     env:{
         BASE_ENDPOINT:"https://api.themoviedb.org/3/",
         API_AUTHORIZATION_TOKEN:"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNGZjMWE0YjA3Y2EzYWVhMmNmODY5ZGRlZGMwOTBjNyIsInN1YiI6IjY0Yjk5NTNmMTEzODZjMDEwYzE4MmU3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n64MkwcoV6EAm-aDu9YzOzf7twtBmfKOPTzivUi9Ikc",
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-   const nounce = randomBytes(128).toString('base64')
+   const nonce = randomBytes(128).toString('base64')
   return (
     <html lang="en">
-      <meta httpEquiv="Content-Security-Policy" content="frame-src https://www.youtube.com https://www.multiembed.mov/ https://www.2embed.cc;"/>
-      {/* <meta httpEquiv="Content-Security-Policy" content={`default-src 'self';'nonce-${nounce}' frame-src 'self' https://www.youtube.com/ https://www.multiembed.mov/ https://www.2embed.cc;`}/> */}
+      <meta httpEquiv={`Content-Security-Policy" content="object-src 'none';base-uri 'none';script-src 'self' 'unsafe-eval' 'unsafe-inline' frame-src https://www.youtube.com https://www.multiembed.mov/ https://www.2embed.cc; htps: http: 'nonce-${nonce}' 'strict-dynamic'`}/>
+     
       <body className={inter.className} suppressHydrationWarning={true} >{children}</body>
     </html>
   )
