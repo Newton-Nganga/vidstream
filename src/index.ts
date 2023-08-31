1dbfd3348abcb1e4a3a59869a5c35d624657f16d
Newton-Nganga <nganga7newt@gmail.com>
2023-08-31 18:41:38 +0300

    env updates

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import router from "./Routes/Routes"
 import bodyParser from "body-parser"
 import cors from 'cors'
 // import "dotenv/config"
+require('dotenv').config()
 
 const app = express()
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 
 // app.use(express.json())
 // app.use(express.urlencoded({extended:true}))
