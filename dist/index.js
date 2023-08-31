06a4606c73e42d41da70d46b6a10665d78012e3e
Newton-Nganga <nganga7newt@gmail.com>
2023-08-31 20:24:50 +0300

    updated port varibles

--- dist/index.js
+++ dist/index.js
@@ -8,8 +8,6 @@ var express_1 = __importDefault(require("express"));
 var Routes_1 = __importDefault(require("./Routes/Routes"));
 var body_parser_1 = __importDefault(require("body-parser"));
 var cors_1 = __importDefault(require("cors"));
-var dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 var app = (0, express_1.default)();
 var PORT = process.env.PORT || 3000;
 // app.use(express.json())
